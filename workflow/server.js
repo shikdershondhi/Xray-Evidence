f48474fb4e1b704c5c01b57df6e227f37a54083c
M. EHATESHAM-UL-ISLAM <ahateshamulislam@gmail.com>
2026-08-25 00:49:45 +0600

    Fix local server detection when app is served from github.io (#25)

--- workflow/server.js
+++ workflow/server.js
@@ -35,6 +35,7 @@ function corsHeaders() {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Private-Network": "true",
   };
 }
 
